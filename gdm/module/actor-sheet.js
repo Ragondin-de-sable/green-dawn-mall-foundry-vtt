7787ee7e614d5c85c7daaae5c0bf89f01fd92efb
Ragondin <gabriel.desa@gmail.com>
2025-12-08 13:30:36 +0100

    style and localization

--- gdm/module/actor-sheet.js
+++ gdm/module/actor-sheet.js
@@ -45,7 +45,7 @@ export class SimpleActorSheet extends ActorSheet {
     // Everything below here is only needed if the sheet is editable
     if ( !this.isEditable ) return;
 
-    html.find("#roll-d6").click(this.openRollDialog.bind(this.actor, this.actor.getRollData()));
+    html.find("#roll-d6").click(this.openActionRollDialog.bind(this.actor));
     // Attribute Management
     // html.find(".attributes").on("click", ".attribute-control", EntitySheetHelper.onClickAttributeControl.bind(this));
     // html.find(".groups").on("click", ".group-control", EntitySheetHelper.onClickAttributeGroupControl.bind(this));
@@ -65,51 +65,61 @@ export class SimpleActorSheet extends ActorSheet {
     // });
   }
 
-  openRollDialog(actor, rollData){
+  openActionRollDialog(){
     const dialogContent = `  
       <div>
-        <i class="fa-solid fa-bandage"></i> Les blessures seront prises en compte automatiquement.
+        <i class="fa-solid fa-bandage"></i> <label>${game.i18n.localize("SIMPLE.ActionDialog.Harms")}</label>
       </div>    
       <hr/>
-      <div>
-        J'utilise l'un de mes traits ? 
-        <input type="radio" id="use-trait-yes" name="use-trait" value="yes"/>
-        <label for="use-trait-yes">Oui</label>
-        <input type="radio" id="use-trait-no" name="use-trait" value="no" checked/>
-        <label for="use-trait-no">Non</label>
+      <div style="display: flex;justify-content: space-between;">
+        <label>${game.i18n.localize("SIMPLE.ActionDialog.Traits")}</label>
+        <span>
+          <input type="radio" id="use-trait-yes" name="use-trait" value="yes"/>
+          <label for="use-trait-yes">${game.i18n.localize("SIMPLE.ActionDialog.Yes")}</label>
+          <input type="radio" id="use-trait-no" name="use-trait" value="no" checked/>
+          <label for="use-trait-no">${game.i18n.localize("SIMPLE.ActionDialog.No")}</label>
+        </span>
       </div>
 
-      <div>
-        J'utilise un de mes objets ? 
-        <input type="radio" id="use-object-yes" name="use-object" value="yes"/>
-        <label for="use-object-yes">Oui</label>
-        <input type="radio" id="use-object-no" name="use-object" value="no" checked/>
-        <label for="use-object-no">Non</label>
-      
-      <div>
-        C'est une circonstance avantageuse ? 
-        <input type="radio" id="advantageous-circumstance-yes" name="advantageous-circumstance" value="yes"/>
-        <label for="advantageous-circumstance-yes">Oui</label>
-        <input type="radio" id="advantageous-circumstance-no" name="advantageous-circumstance" value="no" checked/>
-        <label for="advantageous-circumstance-no">Non</label>
+      <div style="display: flex;justify-content: space-between;">
+        <label>${game.i18n.localize("SIMPLE.ActionDialog.Equipments")}</label>
+        <span>
+          <input type="radio" id="use-object-yes" name="use-object" value="yes"/>
+          <label for="use-object-yes">${game.i18n.localize("SIMPLE.ActionDialog.Yes")}</label>
+          <input type="radio" id="use-object-no" name="use-object" value="no" checked/>
+          <label for="use-object-no">${game.i18n.localize("SIMPLE.ActionDialog.No")}</label>
+        </span>
+      </div>
+      <div style="display: flex;justify-content: space-between;">
+        <label>${game.i18n.localize("SIMPLE.ActionDialog.AdvantageousCircumstance")}</label>
+        <span>
+          <input type="radio" id="advantageous-circumstance-yes" name="advantageous-circumstance" value="yes"/>
+          <label for="advantageous-circumstance-yes">${game.i18n.localize("SIMPLE.ActionDialog.Yes")}</label>
+          <input type="radio" id="advantageous-circumstance-no" name="advantageous-circumstance" value="no" checked/>
+          <label for="advantageous-circumstance-no">${game.i18n.localize("SIMPLE.ActionDialog.No")}</label>
+        </span>
       </div>
 
       <hr />
 
-      <div>
-        Un autre PJ m'aide avec l'un de ses objets ? 
-        <input type="radio" id="other-pj-help-object-yes" name="other-pj-help-object" value="yes"/>
-        <label for="other-pj-help-object-yes">Oui</label>
-        <input type="radio" id="other-pj-help-object-no" name="other-pj-help-object" value="no" checked/>
-        <label for="other-pj-help-object-no">Non</label>
+      <div style="display: flex;justify-content: space-between;">
+        <label>${game.i18n.localize("SIMPLE.ActionDialog.OtherPJHelpObject")}</label>
+        <span>
+          <input type="radio" id="other-pj-help-object-yes" name="other-pj-help-object" value="yes"/>
+          <label for="other-pj-help-object-yes">${game.i18n.localize("SIMPLE.ActionDialog.Yes")}</label>
+          <input type="radio" id="other-pj-help-object-no" name="other-pj-help-object" value="no" checked/>
+          <label for="other-pj-help-object-no">${game.i18n.localize("SIMPLE.ActionDialog.No")}</label>
+        </span>
       </div>
 
-      <div>
-        Un autre PJ m'aide avec l'un de ses traits ? 
-        <input type="radio" id="other-pj-help-trait-yes" name="other-pj-help-trait" value="yes"/>
-        <label for="other-pj-help-trait-yes">Oui</label>
-        <input type="radio" id="other-pj-help-trait-no" name="other-pj-help-trait" value="no" checked/>
-        <label for="other-pj-help-trait-no">Non</label>
+      <div style="display: flex;justify-content: space-between;">
+        <label>${game.i18n.localize("SIMPLE.ActionDialog.OtherPJHelpTrait")}</label>
+        <span>
+          <input type="radio" id="other-pj-help-trait-yes" name="other-pj-help-trait" value="yes"/>
+          <label for="other-pj-help-trait-yes">${game.i18n.localize("SIMPLE.ActionDialog.Yes")}</label>
+          <input type="radio" id="other-pj-help-trait-no" name="other-pj-help-trait" value="no" checked/>
+          <label for="other-pj-help-trait-no">${game.i18n.localize("SIMPLE.ActionDialog.No")}</label>
+        </span>
       </div>
       
       <hr />`;
@@ -121,7 +131,7 @@ export class SimpleActorSheet extends ActorSheet {
         confirm: {
           icon: '<i class="fa-solid fa-dice"></i>',
           label: game.i18n.localize("SIMPLE.ActionDialog.Confirm"),
-          callback: (html) => confirmCallback(html, actor.harms),
+          callback: (html) => confirmCallback(html, this),
         },
         cancel: {
           icon: '<i class="fa-solid fa-ban"></i>',
@@ -130,15 +140,19 @@ export class SimpleActorSheet extends ActorSheet {
       }
     }).render(true);
 
-    function confirmCallback(html, harms) {
-      var nbDice = getNbDice(html, harms);
+    async function confirmCallback(html, simpleActor) {
+      var nbDice = getNbDice(html, simpleActor.system.harms);
   
-      let r = new Roll(nbDice+"d6", rollData);
+      let r = new Roll(nbDice+"d6", simpleActor.getRollData());
+      const rollResult = await r.evaluate();
+      const isSuccess = rollResult.terms[0].results.find(e => e.result >= 5) !== undefined;
 
       return r.toMessage({
         user: game.user.id,
-        speaker: ChatMessage.getSpeaker({ actor: actor }),
-        flavor: `<h2>${actor.name}</h2><h3>${game.i18n.localize("SIMPLE.RollD6")}</h3>`
+        speaker: ChatMessage.getSpeaker({ actor: simpleActor }),
+        flavor: isSuccess 
+          ? `<div style="background-color:green; color: white; text-align: center; font-weight: bold;font-size: large;">${game.i18n.localize("SIMPLE.ActionDialog.Success")}</div>` 
+          : `<div style="background-color:red; color: white;text-align: center; font-weight: bold;font-size: large;">${game.i18n.localize("SIMPLE.ActionDialog.Failure")}</div>`
       });
     }
 
@@ -152,7 +166,6 @@ export class SimpleActorSheet extends ActorSheet {
       if(harms.severe.trim() !== "")
         nbDice--;
 
-      
       if (html.find("input#use-trait-yes").is(":checked"))
         nbDice++;
 
@@ -173,6 +186,8 @@ export class SimpleActorSheet extends ActorSheet {
 
       if(nbDice <= 0)
         nbDice = 1;
+
+      //todo other rules
 
       return nbDice;
     }
